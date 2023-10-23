32646162216bdd47faa64934b3ceaa6e760762cd
Alexander Shavrov <alexander.shavrov@mail.ru>
2023-10-23 13:07:44 +0300

    Update entry point

--- src/main/java/com/wingmann/saqra/Main.java
+++ src/main/java/com/wingmann/saqra/Main.java
@@ -4,16 +4,28 @@ import com.wingmann.saqra.config.ApplicationConfig;
 import com.wingmann.saqra.config.Config;
 import com.wingmann.saqra.generator.Generator;
 import com.wingmann.saqra.generator.QrGenerator;
+import com.wingmann.saqra.generator.QrImageCache;
+import com.wingmann.saqra.io.ImageWriter;
+import com.wingmann.saqra.io.Writer;
+
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         System.out.print("Saqra v1.0.0 (qr code generator)\n\n");
 
         Config config = new ApplicationConfig("./config.txt");
+
         Generator generator = new QrGenerator(config);
+        Writer writer = new ImageWriter(config);
+
+        Optional<QrImageCache> result;
 
         while (true) {
-            if (generator.build()) {
+            result = generator.build();
+
+            if (result.isPresent()) {
+                writer.write(result.get());
                 continue;
             }
             break;
